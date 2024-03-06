785552f193830e8568e4f06069d288163ce04376
moaoa <moaadbn3@gmail.com>
2024-03-07 01:39:32 +0200

    add the localStorage for the student Store

--- src/router/index.js
+++ src/router/index.js
@@ -7,10 +7,9 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'login',
-      component: LoginView
-      
+      path: "/",
+      name: "login",
+      component: LoginView,
     },
     {
       path: "/teacher",
@@ -23,99 +22,105 @@ const router = createRouter({
       component: LoginView,
     },
     {
-      path: '/home',
-      name: 'home',
-      component: () => import('../views/StudentHome.vue'),
-      beforeEnter: (to, from, next) => {
-        const student = useStudentStore()
-        if ((
-          from.path === '/' ||
-          from.path === '/absenceRatio' ||
-          from.path === '/grades') && student.isAuthenticated) {
-          next();
-        } else {
-          next('/');
-        }
-      },
-     
+      path: "/home",
+      name: "home",
+      component: () => import("../views/StudentHome.vue"),
+      // beforeEnter: (to, from, next) => {
+      //   const student = useStudentStore();
+      //   if (
+      //     (from.path === "/" ||
+      //       from.path === "/absenceRatio" ||
+      //       from.path === "/grades") &&
+      //     student.isAuthenticated
+      //   ) {
+      //     next();
+      //   } else {
+      //     next("/");
+      //   }
+      // },
     },
     {
-      path: '/absenceRatio',
-      name: 'absenceRatio',
-      component: () => import('../views/StudentAbsenceRatio.vue'),
-      beforeEnter: (to, from, next) => {
-        if (from.path === '/home'||from.path === '/grades') {
-          next();
-        } else {
-          next('/');
-        }
-      },
+      path: "/absenceRatio",
+      name: "absenceRatio",
+      component: () => import("../views/StudentAbsenceRatio.vue"),
+      // beforeEnter: (to, from, next) => {
+      //   if (from.path === "/home" || from.path === "/grades") {
+      //     next();
+      //   } else {
+      //     next("/");
+      //   }
+      // },
     },
     {
-      path: '/grades',
-      name: 'grades',
-      component: () => import('../views/StudentGrade.vue'),
-      beforeEnter: (to, from, next) => {
-        if (from.path === '/home'||from.path === '/absenceRatio') {
-          next();
-        } else {
-          next('/');
-        }
-      },
+      path: "/grades",
+      name: "grades",
+      component: () => import("../views/StudentGrade.vue"),
+      // beforeEnter: (to, from, next) => {
+      //   if (from.path === "/home" || from.path === "/absenceRatio") {
+      //     next();
+      //   } else {
+      //     next("/");
+      //   }
+      // },
     },
     {
-      path: '/teacher/home',
-      name: 'teacherHome',
-      component: () => import('../views/TeacherHome.vue'),
-      beforeEnter: (to, from, next) => {
-        const teacher = useTeacherStore()
-        if ((
-          from.path === '/teacher' ||
-          from.path === '/teacher/attendence'||
-          from.path === '/teacher/subjects' ||
-          from.path === '/teacher/grades')
-           && teacher.isAuthenticated) {
-          next();
-        } else {
-          next('/teacher');
-        }
-      },
+      path: "/teacher/home",
+      name: "teacherHome",
+      component: () => import("../views/TeacherHome.vue"),
+      // beforeEnter: (to, from, next) => {
+      //   const teacher = useTeacherStore();
+      //   if (
+      //     (from.path === "/teacher" ||
+      //       from.path === "/teacher/attendence" ||
+      //       from.path === "/teacher/subjects" ||
+      //       from.path === "/teacher/grades") &&
+      //     teacher.isAuthenticated
+      //   ) {
+      //     next();
+      //   } else {
+      //     next("/teacher");
+      //   }
+      // },
     },
     {
-      path: '/teacher/attendence',
-      name: 'StudentAttendence',
-      component: () => import('../views/StudentAttendence.vue'),
-      beforeEnter: (to, from, next) => {
-        if (from.path === '/teacher/home') {
-          next();
-        } else {
-          next('/teacher');
-        }
-      },
+      path: "/teacher/attendence",
+      name: "StudentAttendence",
+      component: () => import("../views/StudentAttendence.vue"),
+      // beforeEnter: (to, from, next) => {
+      //   if (from.path === "/teacher/home") {
+      //     next();
+      //   } else {
+      //     next("/teacher");
+      //   }
+      // },
     },
     {
-      path: '/teacher/subjects',
-      name: 'TeacherSubjects',
-      component: () => import('../views/TeacherSubjects.vue'),
-      beforeEnter: (to, from, next) => {
-        if (from.path === '/teacher/home'||from.path === '/teacher/attendence'||from.path === '/teacher/grades') {
-          next();
-        } else {
-          next('/teacher');
-        }
-      },
+      path: "/teacher/subjects",
+      name: "TeacherSubjects",
+      component: () => import("../views/TeacherSubjects.vue"),
+      // beforeEnter: (to, from, next) => {
+      //   if (
+      //     from.path === "/teacher/home" ||
+      //     from.path === "/teacher/attendence" ||
+      //     from.path === "/teacher/grades"
+      //   ) {
+      //     next();
+      //   } else {
+      //     next("/teacher");
+      //   }
+      // },
     },
     {
-      path: '/teacher/grades',
-      name: 'TeacherGrades',
-      component: () => import('../views/TeacherStudentGrades.vue'),
-      beforeEnter: (to, from, next) => {
-        if (from.path === '/teacher/subjects') {
-          next();
-        } else {
-          next('/teacher');
-        }
-      },
+      path: "/teacher/grades",
+      name: "TeacherGrades",
+      component: () => import("../views/TeacherStudentGrades.vue"),
+      // beforeEnter: (to, from, next) => {
+      //   if (from.path === "/teacher/subjects") {
+      //     next();
+      //   } else {
+      //     next("/teacher");
+      //   }
+      // },
     },
     {
       path: "/admin/home",
@@ -132,6 +137,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/Admin/CreateStudent.vue"),
+    },
+    {
+      path: "/admin/create-lecture",
+      name: "adminCreateLecture",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/Admin/CreateLecture.vue"),
     },
     {
       path: "/admin/create-teacher",
--- src/stores/StudentStore.js
+++ src/stores/StudentStore.js
@@ -1,68 +1,62 @@
-import { defineStore } from "pinia"
-import CetApi from '../Repository/CetApi'
+import { defineStore } from "pinia";
+import CetApi from "../Repository/CetApi";
 import { useLocalStorage } from "@vueuse/core";
 
+export const useStudentStore = defineStore("StudentStore", {
+  state: () => ({
+    Data: useLocalStorage("studentInfo", {}),
+    isAuthenticated: false,
+    schedule: null,
+    absenceRatio: null,
+    grades: null,
+    isLoading: false,
+  }),
+  actions: {
+    async login(data) {
+      try {
+        this.isLoading = true;
+        const response = await CetApi.studentLogin(data);
+        this.Data = response.data;
 
-export const useStudentStore = defineStore('StudentStore', {
-    state: () => {
-        // Data: useLocalStorage("studentData", {});
-        Data: JSON.parse(localStorage.getItem("studentData")) || {}; // Initialize with local storage data or an empty object
-        isAuthenticated:false;
-        schedule: null;
-        absenceRatio: null;
-        grades: null;
-        isLoading: false;
+        this.isAuthenticated = true;
+      } catch (error) {
+        throw error.response.status;
+      } finally {
+        this.isLoading = false;
+      }
     },
-    actions: {
-
-        async login(data) {
-            try {
-                this.isLoading = true;
-                const response = await CetApi.studentLogin(data);
-                this.Data = response.data;
-                localStorage.setItem('studentData', JSON.stringify(response.data));
-                if (response.status == 200) {
-                    this.isAuthenticated = true;
-                }
-            } catch (error) {
-                throw error.response.status
-            }finally{
-                this.isLoading =false
-            }
-        },
-        async getSchedule(id) {
-            try {
-                this.isLoading = true
-                const response = await CetApi.studentSchedule(id)
-                this.schedule = response.data
-            } catch (error) {
-                throw error
-            } finally {
-                this.isLoading = false
-            }
-        },
-        async getAbsenceRatio(id) {
-            try {
-                this.loading = true;
-                const response = await CetApi.absenseRatio(id);
-                this.absenceRatio = response.data;
-            } catch (error) {
-                throw error;
-            } finally {
-                this.loading = false;
-            }
-        },
-        async getGrades(id) {
-            try {
-                this.loading = true;
-                const response = await CetApi.studentGrades(id);
-                this.grades = response.data;
-            } catch (error) {
-                throw error;
-            } finally {
-                this.loading = false;
-            }
-        },
+    async getSchedule(id) {
+      try {
+        this.isLoading = true;
+        const response = await CetApi.studentSchedule(id);
+        this.schedule = response.data;
+      } catch (error) {
+        throw error;
+      } finally {
+        this.isLoading = false;
+      }
     },
-
-})
+    async getAbsenceRatio(id) {
+      try {
+        this.loading = true;
+        const response = await CetApi.absenseRatio(id);
+        this.absenceRatio = response.data;
+      } catch (error) {
+        throw error;
+      } finally {
+        this.loading = false;
+      }
+    },
+    async getGrades(id) {
+      try {
+        this.loading = true;
+        const response = await CetApi.studentGrades(id);
+        this.grades = response.data;
+      } catch (error) {
+        throw error;
+      } finally {
+        this.loading = false;
+      }
+    },
+  },
+});
