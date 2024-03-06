6411413fa034bbdbe2e6201d92f2253ba145a564
Ahmed graish <graish333@gmail.com>
2024-03-07 00:46:43 +0200

    completed the create teacher layout

--- src/views/Admin/CreateTeacher.vue
+++ src/views/Admin/CreateTeacher.vue
@@ -5,41 +5,43 @@ import { string, object, z } from "zod";
 import { computed, ref } from "vue";
 import InputField from "@/components/InputField.vue";
 import axios from "axios";
-import router from "@/router";
 import Button from "@/components/Button.vue";
 import { useAdminStore } from "@/stores/AdminStore";
 import AdminSideBar from "@/components/SideBar.vue";
-import scheduleIcon from "@/components/icons/IconSchedule.vue";
+import addLectureIcon from "@/components/icons/IconSchedule.vue";
 import percentageIcon from "@/components/icons/IconPercentage.vue";
 import marksIcon from "@/components/icons/IconMarks.vue";
 import settingsIcon from "@/components/icons/IconSittings.vue";
-
+import AddStudentIcon from "@/components/icons/IconAddStudent.vue";
+import AddTeacherIcon from "@/components/icons/IconAddTeacher.vue";
+import submitBtn from "@/components/SubmitBtn.vue";
 const studentNav = [
-  { name: 1, icon: scheduleIcon, route: "/home" },
-  { name: 2, icon: percentageIcon, route: "/absenceRatio" },
-  { name: 3, icon: marksIcon, route: "/grades" },
-  { name: 4, icon: settingsIcon, route: "" },
+  { name: 1, icon: AddTeacherIcon, route: "" },
+  { name: 2, icon: AddStudentIcon, route: "" },
+  { name: 3, icon: addLectureIcon, route: "" },
+  { name: 4, icon: marksIcon, route: "" },
 ];
 const adminStore = useAdminStore();
 
 const schema = object({
-  ref_number: string().min(4, "يجب أن يتكون الرقم الوطني من 4 رقم").max(12),
-  name: string(),
-  password: string().min(8),
-  email: string().email(),
-  phone_number: string(),
-  confirmPassword: string().min(8),
+  ref_number: string({ required_error: "مطلوب" })
+    .min(4, "يجب ان يتكون معرف الاستاذ من اربعة ارقام")
+    .max(12),
+  name: string({ required_error: "مطلوب" }),
+  password: string({ required_error: "مطلوب" }).min(
+    8,
+    "يجب ان تتكون كلمة المرور من 8 حروف"
+  ),
+  email: string({ required_error: "مطلوب" }).email("الرجاء ادخال ايميل صحيح"),
+  phone_number: string({ required_error: "مطلوب" }),
+  confirmPassword: string({ required_error: "مطلوب" }).min(
+    8,
+    "يجب ان تتكون كلمة المرور من 8 حروف"
+  ),
 }).refine((data) => data.password === data.confirmPassword, {
   message: "يجب على كلمة المرور ان تكون مطابقة",
   path: ["confirmPassword"],
 });
-
-// const frontendKeysToBackendKeys = {
-//   ref_number: "national_id",
-//   name: "name_number",
-//   password: "password",
-//   confirmPassword: "password_confirmation",
-// };
 
 const typedSchema = toTypedSchema(schema);
 
@@ -79,126 +81,105 @@ const isConfirmPasswordHidden = ref(true);
   <div class="mainContainer">
     <div class="body">
       <main>
-        <div>
-          <div>
-            <h1>إنشاء حساب استاذ</h1>
+        <h1>إنشاء حساب استاذ</h1>
+        <form @submit="onSubmit" :validation-schema="typedSchema" action="#" dir="rtl">
+          <div class="inputMianContainer">
+            <div class="inputContainer">
+              <label for="ref_number"> رقم الاشاري </label>
+              <Field type="text" name="ref_number" id="ref_number" v-slot="{ field }">
+                <InputField
+                  type="text"
+                  v-bind="field"
+                  placeholder="xxxx"
+                  v-mask="'####'"
+                />
+              </Field>
+              <ErrorMessage name="ref_number" as="small" class="hint" />
+            </div>
+            <div class="inputContainer">
+              <label for="password"> الاسم </label>
+              <Field name="name" v-slot="{ field }">
+                <InputField id="name" type="text" v-bind="field" placeholder="الاسم">
+                  <template #postfix>
+                    <div v-if="isFieldTouched('name')">
+                      <CheckIcon v-if="isFieldValid('name')" />
+                      <CrossIcon v-else />
+                    </div>
+                  </template>
+                </InputField>
+              </Field>
+              <ErrorMessage name="name" as="small" class="hint" />
+            </div>
+            <div class="inputContainer">
+              <label for="phone_number"> رقم الهاتف </label>
+              <Field name="phone_number" v-slot="{ field }">
+                <InputField
+                  id="phone_number"
+                  type="text"
+                  v-bind="field"
+                  placeholder="phone_number"
+                />
+              </Field>
+              <ErrorMessage name="phone_number" as="small" class="hint" />
+            </div>
+            <div class="inputContainer">
+              <label for="email"> الايميل </label>
+              <Field name="email" v-slot="{ field }">
+                <InputField id="email" type="text" v-bind="field" placeholder="email" />
+              </Field>
+              <ErrorMessage name="email" as="small" class="hint" />
+            </div>
+            <div class="inputContainer">
+              <label for="password"> كلمة المرور </label>
+              <Field
+                type="password"
+                name="password"
+                id="password"
+                placeholder="xxxxxxxxxxxx"
+                v-slot="{ field }"
+              >
+                <InputField
+                  :type="isPassword ? 'password' : 'text'"
+                  v-bind="field"
+                  placeholder="كلمة السر"
+                >
+                  <template #postfix>
+                    <div @click="isPassword = !isPassword">
+                      <EyeIcon v-if="isPassword" />
+                      <EyeOffIcon v-else />
+                    </div>
+                  </template>
+                </InputField>
+              </Field>
+              <ErrorMessage name="password" as="small" class="hint" />
+            </div>
+            <div class="inputContainer">
+              <label for="confirmPassword"> تأكيد كلمة المرور </label>
+              <Field
+                type="password"
+                name="confirmPassword"
+                id="confirmPassword"
+                placeholder="xxxxxxxxxxxx"
+                v-slot="{ field }"
+              >
+                <InputField
+                  :type="isConfirmPasswordHidden ? 'password' : 'text'"
+                  v-bind="field"
+                  placeholder="تأكيد كلمة المرور "
+                >
+                  <template #postfix>
+                    <div @click="isConfirmPasswordHidden = !isConfirmPasswordHidden">
+                      <EyeIcon v-if="isConfirmPasswordHidden" />
+                      <EyeOffIcon v-else />
+                    </div>
+                  </template>
+                </InputField>
+              </Field>
+              <ErrorMessage name="confirmPassword" as="small" class="hint" />
+            </div>
           </div>
-          <div>
-            <form
-              @submit="onSubmit"
-              :validation-schema="typedSchema"
-              action="#"
-              dir="rtl"
-            >
-              <div>
-                <div>
-                  <label for="ref_number"> رقم الاشاري </label>
-                  <Field type="text" name="ref_number" id="ref_number" v-slot="{ field }">
-                    <InputField
-                      type="text"
-                      v-bind="field"
-                      placeholder="xxxx"
-                      v-mask="'####'"
-                    />
-                  </Field>
-                  <ErrorMessage name="ref_number" as="small" />
-                </div>
-                <div>
-                  <label for="password"> الاسم </label>
-                  <Field name="name" v-slot="{ field }">
-                    <InputField id="name" type="text" v-bind="field" placeholder="الاسم">
-                      <template #postfix>
-                        <div v-if="isFieldTouched('name')">
-                          <CheckIcon v-if="isFieldValid('name')" />
-                          <CrossIcon v-else />
-                        </div>
-                      </template>
-                    </InputField>
-                  </Field>
-                  <ErrorMessage name="name" as="small" />
-                </div>
-                <div>
-                  <label for="phone_number"> رقم الهاتف </label>
-                  <Field name="phone_number" v-slot="{ field }">
-                    <InputField
-                      id="phone_number"
-                      type="text"
-                      v-bind="field"
-                      placeholder="phone_number"
-                    />
-                  </Field>
-                  <ErrorMessage name="phone_number" as="small" />
-                </div>
-                <div>
-                  <label for="email"> الايميل </label>
-                  <Field name="email" v-slot="{ field }">
-                    <InputField
-                      id="email"
-                      type="text"
-                      v-bind="field"
-                      placeholder="email"
-                    />
-                  </Field>
-                  <ErrorMessage name="email" as="small" />
-                </div>
-                <div>
-                  <label for="password"> كلمة المرور </label>
-                  <Field
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="xxxxxxxxxxxx"
-                    v-slot="{ field }"
-                  >
-                    <InputField
-                      :type="isPassword ? 'password' : 'text'"
-                      v-bind="field"
-                      placeholder="كلمة السر"
-                    >
-                      <template #postfix>
-                        <div @click="isPassword = !isPassword">
-                          <EyeIcon v-if="isPassword" />
-                          <EyeOffIcon v-else />
-                        </div>
-                      </template>
-                    </InputField>
-                  </Field>
-                  <ErrorMessage name="password" as="small" />
-                </div>
-                <div>
-                  <label for="confirmPassword"> تأكيد كلمة المرور </label>
-                  <Field
-                    type="password"
-                    name="confirmPassword"
-                    id="confirmPassword"
-                    placeholder="xxxxxxxxxxxx"
-                    v-slot="{ field }"
-                  >
-                    <InputField
-                      :type="isConfirmPasswordHidden ? 'password' : 'text'"
-                      v-bind="field"
-                      placeholder="تأكيد كلمة المرور "
-                    >
-                      <template #postfix>
-                        <div @click="isConfirmPasswordHidden = !isConfirmPasswordHidden">
-                          <EyeIcon v-if="isConfirmPasswordHidden" />
-                          <EyeOffIcon v-else />
-                        </div>
-                      </template>
-                    </InputField>
-                  </Field>
-                  <ErrorMessage name="confirmPassword" as="small" />
-                </div>
-              </div>
-              <Button type="submit" :disabled="isLoading" size="full">
-                <span v-if="isLoading">
-                  <LoadingIcon />
-                </span>
-                <span v-else> التسجيل </span>
-              </Button>
-            </form>
-          </div>
-        </div>
+          <submitBtn value="حفظ" id="submitBtn" />
+        </form>
       </main>
     </div>
 
@@ -217,8 +198,6 @@ const isConfirmPasswordHidden = ref(true);
 }
 .body {
   display: flex;
-  flex-direction: column;
-  justify-content: space-between;
   position: relative;
   align-items: end;
   height: 100%;
@@ -230,7 +209,36 @@ main {
   align-items: center;
   position: relative;
   width: 100%;
-  height: 85%;
-  padding: 0 5% 2%;
+  height: 100%;
+}
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+.inputMianContainer {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  position: relative;
+  width: 100%;
+  height: 75%;
+  margin-top: 3%;
+}
+.inputContainer {
+  width: 40%;
+}
+#submitBtn {
+  width: 30%;
+  height: 10%;
+  margin-bottom: -20%;
+}
+.hint {
+  margin: 2% 2% 0 0;
+  font-size: medium;
+  color: crimson;
 }
 </style>
